e9db80bc242674472cab08c403c140c87d6acd22
Aman Tomar <143558906+aman420xd@users.noreply.github.com>
2025-05-24 04:34:28 +0530

    Initial commit: Add existing project files

---
+++ backend/src/lib/db.js
@@ -0,0 +1,12 @@
+import mongoose from 'mongoose'
+export const connectDB = async ()=>{
+  try{
+    const conn= await mongoose.connect(process.env.MONGODB_URI)
+ console.log(`mongoDB connected : ${conn.connection.host}`)
+  
+  }
+  catch{
+    console.log("mongodb connessction error", error)
+  }
+}
+
